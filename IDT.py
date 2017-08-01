0694db2b90dd6be81201b0b33d9c2e74e02a8b92
Jonathan Lisinski <jonathan.lisinski@gmail.com>
2017-08-01 15:31:29 -0400

    Changed license to ECLv2

--- IDT.py
+++ IDT.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 
 """
-IDT: Individualized (Monetary) Discounting Task
+IDT: Individualized Discounting Task
 
-Copyright (C) 2017 - 2017 Mikhail Koffarnus (mickyk@vtc.vt.edu)
 
-Distributed under the terms of the GNU Lesser General Public License
-(LGPL). See LICENSE.TXT that came with this file.
+Copyright 2017  Mikhail Koffarnus. 
+
+Licensed under the Educational Community License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may
+obtain a copy of the License at: http://www.osedu.org/licenses/ECL-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an "AS IS"
+BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied. See the License for the specific language governing
+permissions and limitations under the License.
 """
-
 
 import csv
 import pygame
